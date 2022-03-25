f300c71ac79e5bbd235b7b276784f38dd9808d0e
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-03-25 00:16:18 +0000

    Code.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Samuel Carlson All rights reserved
+//
+// Created by: Samuel Carlson
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function enterClicked () {
+  // this function gets user's street name and street number and displays it back to user
+
+  // get street name from text field
+  let streetName = document.getElementById("street-name").value
+	// get street number from text field and cast it to integer
+  let streetNumber = parseInt(document.getElementById("street-number$-entered").value)
+
+  // display street name and street number back to user
+  document.getElementById('user-info').innerHTML = "Your street name is " + streetName + " and your street number is " + streetName + " . "
+}
